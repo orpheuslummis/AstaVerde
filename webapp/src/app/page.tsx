0991e1267d00a72972332b87db7fe4cf0bf0bf6e
Orpheus Lummis <o@orpheuslummis.info>
2023-11-14 12:15:34 +0300

    nextjs compliance

--- webapp/src/app/page.tsx
+++ webapp/src/app/page.tsx
@@ -1,23 +1,23 @@
-import { Account } from '../components/Account'
-import { Balance } from '../components/Balance'
-import { BlockNumber } from '../components/BlockNumber'
-import { ConnectKitButton } from '../components/ConnectKitButton'
-import { Connected } from '../components/Connected'
-import { NetworkSwitcher } from '../components/NetworkSwitcher'
-import { ReadContract } from '../components/ReadContract'
-import { ReadContracts } from '../components/ReadContracts'
-import { ReadContractsInfinite } from '../components/ReadContractsInfinite'
-import { SendTransaction } from '../components/SendTransaction'
-import { SendTransactionPrepared } from '../components/SendTransactionPrepared'
-import { SignMessage } from '../components/SignMessage'
-import { SignTypedData } from '../components/SignTypedData'
-import { Token } from '../components/Token'
-import { WatchContractEvents } from '../components/WatchContractEvents'
-import { WatchPendingTransactions } from '../components/WatchPendingTransactions'
-import { WriteContract } from '../components/WriteContract'
-import { WriteContractPrepared } from '../components/WriteContractPrepared'
+import { Account } from "../components/Account";
+import { Balance } from "../components/Balance";
+import { BlockNumber } from "../components/BlockNumber";
+import { ConnectKitButton } from "../components/ConnectKitButton";
+import { Connected } from "../components/Connected";
+import { NetworkSwitcher } from "../components/NetworkSwitcher";
+import { ReadContract } from "../components/ReadContract";
+import { ReadContracts } from "../components/ReadContracts";
+import { ReadContractsInfinite } from "../components/ReadContractsInfinite";
+import { SendTransaction } from "../components/SendTransaction";
+import { SendTransactionPrepared } from "../components/SendTransactionPrepared";
+import { SignMessage } from "../components/SignMessage";
+import { SignTypedData } from "../components/SignTypedData";
+import { Token } from "../components/Token";
+import { WatchContractEvents } from "../components/WatchContractEvents";
+import { WatchPendingTransactions } from "../components/WatchPendingTransactions";
+import { WriteContract } from "../components/WriteContract";
+import { WriteContractPrepared } from "../components/WriteContractPrepared";
 
-export function Page() {
+export default function Page() {
   return (
     <>
       <h1>Asta Verde</h1>
@@ -90,7 +90,5 @@ export function Page() {
         <WriteContractPrepared />
       </Connected>
     </>
-  )
+  );
 }
-
-export default Page
