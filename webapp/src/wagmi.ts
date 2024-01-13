1883fc4483b96a2559dabd8b189f72c3bb1f1d59
Orpheus Lummis <o@orpheuslummis.info>
2024-01-13 22:11:50 +0700

    feat: deployment on testnet Base

--- webapp/src/wagmi.ts
+++ webapp/src/wagmi.ts
@@ -1,65 +1,22 @@
-import { infuraProvider } from "@wagmi/core/providers/infura";
-// import { publicProvider } from "@wagmi/core/providers/public";
 import { getDefaultConfig } from "connectkit";
-// import { createPublicClient, http } from "viem";
-import { configureChains, createConfig, mainnet, sepolia } from "wagmi";
-
-// import dotenv from "dotenv";
-
-// dotenv.config({ path: "../.env" });
+import { configureChains, createConfig } from "wagmi";
+import { base, baseSepolia } from "wagmi/chains";
 
 const walletConnectProjectId = "0ae1a048225917dfb3d8047f884465f1";
-
-// const infuraApiKey = process.env.INFURA_API_KEY;
-const infuraApiKey = "eda31e892a144ca89dd77c572220258a";
-// console.log("INFURA_API_KEY", infuraApiKey);
-// if (!infuraApiKey) {
-//   throw new Error("INFURA_API_KEY is not present in the environment variables.");
-// }
+const alchemyAPIKey = "iLBP5cYsA21h9sQw1rBl9_n7sng_zlMZ";
+const nodeHTTPSURL = "https://base-sepolia.g.alchemy.com/v2/";
+const nodeWSSURL = "wss://base-sepolia.g.alchemy.com/v2/";
 
 const isDev = true;
 console.log("isDev", isDev);
-const chain = isDev ? sepolia : mainnet;
-
-const { chains, publicClient } = configureChains(
-  [chain],
-  [infuraProvider({ apiKey: infuraApiKey })],
-);
+const chain = isDev ? baseSepolia : base;
 
 export const config = createConfig(
   getDefaultConfig({
     autoConnect: true,
     appName: "Asta Verde",
     walletConnectProjectId,
-    publicClient,
-    chains,
-  }),
+    chains: [chain],
+    alchemyId: alchemyAPIKey,
+  })
 );
-
-// export const config = createConfig({
-//   autoConnect: true,
-//   publicClient,
-// });
-
-// export const config = createConfig({
-//   autoConnect: true,
-//   publicClient: createPublicClient({
-//     chain: mainnet,
-//     transport: http(),
-//   }),
-// });
-
-// export const config = createConfig(
-//   {
-//     autoConnect: true,
-//     publicClient,
-//     webSocketPublicClient,
-//   },
-
-// getDefaultConfig({
-//   autoConnect: true,
-//   appName: "Asta Verde",
-//   walletConnectProjectId,
-//   infuraId: infuraApiKey,
-// }),
-// );
