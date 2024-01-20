98400b0978ec34222e8c0a3c18fa208e9c8a9719
Orpheus Lummis <o@orpheuslummis.info>
2024-01-20 16:44:55 +0700

    feat: Add draft text content

--- webapp/src/app.config.ts
+++ webapp/src/app.config.ts
@@ -3,6 +3,7 @@ export const metadata = {
 };
 
 export const navigationLinks = [
+  { name: "Everything about EcoAssets", url: "/ecoassets" },
   { name: "Market", url: "/" },
   { name: "Redeem", url: "/mytokens" },
   { name: "About", url: "/about" },
