e186efc2bf262f7492164c580fdcae4ecfaeec80
aeither <36173828+aeither@users.noreply.github.com>
2024-01-20 20:23:25 +0800

    admin controls (#40)
    * set price floor
    
    * base price
    
    * max batch size
    
    * auction time threshold
    
    * Platform Share Percentage
    
    * Update AdminControls.tsx
    
    * logo and colors (#32)
    
    * add logo
    
    * colors
    
    * format output (#33)
    
    * fix logs
    
    * improve and add instrructions
    
    * format output
    
    * add guide on readme and script in pkg
    
    * allow only numbers
    
    * refresh after approval
    
    * Revert "refresh after approval"
    
    This reverts commit 08223892750191dcaffde9f96cbf5eab8104fbd1.
    
    * check Increase threshold must be lower than decrease threshold

--- webapp/src/components/AdminControls.tsx
+++ webapp/src/components/AdminControls.tsx
@@ -13,17 +13,11 @@ export function AdminControls() {
     <Connected>
       <h2 className="text-2xl my-6 mx-6">Admin controls</h2>
       <div>
-        {/*
-        TODO
         <PlatformPercentageControl />
-          setPlatformSharePercentage
-        <PriceFloorControl />
-          setPriceFloor
-        <BasePriceControl />
-          setBasePrice
+        <AuctionTimeThresholdsControl />
         <MaxBatchSizeControl />
-          setMaxBatchSize
-        */}
+        <PriceFloorControl />
+        <BasePriceControl />
         <ClaimPlatformFunds />
         <PauseContractControl />
         <SetURI />
@@ -178,6 +172,278 @@ function SetURI() {
         </button>
       </div>
       {currentURI && <div className="text-gray-500 mb-2">Current URI: {currentURI}</div>}
+      {isLoading && <div className="text-gray-500">Processing... Please check your wallet.</div>}
+      {isSuccess && <div className="text-primary">Transaction successful</div>}
+      {error && <div className="text-red-500">Error: {error.message}</div>}
+    </ControlContainer>
+  );
+}
+
+function PriceFloorControl() {
+  const [priceFloor, setPriceFloor] = useState("");
+  const { data: currentPriceFloor } = useContractRead({
+    ...astaverdeContractConfig,
+    functionName: "priceFloor",
+  });
+  console.log("currentPriceFloor", currentPriceFloor);
+  const { config } = usePrepareContractWrite({
+    ...astaverdeContractConfig,
+    functionName: "setPriceFloor",
+    args: [BigInt(priceFloor)],
+  });
+  const { write, isLoading, isSuccess, error } = useContractWrite(config);
+  console.log("setPriceFloor", { config, write, isLoading, isSuccess, error });
+
+  const handleSetPriceFloor = () => {
+    if (priceFloor) {
+      write?.();
+    }
+  };
+
+  return (
+    <ControlContainer title="Set Price Floor">
+      <div className="flex items-center mb-4">
+        <input
+          type="number"
+          value={priceFloor}
+          onChange={(e) => setPriceFloor(e.target.value)}
+          placeholder="Enter Price Floor"
+          className="px-4 py-2 mr-2 border border-gray-300 rounded"
+        />
+        <button
+          className="px-4 py-2 bg-secondary text-white rounded hover:bg-blue-700 disabled:opacity-50"
+          disabled={!write || isLoading}
+          onClick={handleSetPriceFloor}
+        >
+          Set Price Floor
+        </button>
+      </div>
+      {currentPriceFloor !== undefined && (
+        <div className="text-gray-500 mb-2">Current Price floor: {currentPriceFloor.toString()}</div>
+      )}
+      {isLoading && <div className="text-gray-500">Processing... Please check your wallet.</div>}
+      {isSuccess && <div className="text-primary">Transaction successful</div>}
+      {error && <div className="text-red-500">Error: {error.message}</div>}
+    </ControlContainer>
+  );
+}
+
+function BasePriceControl() {
+  const [basePrice, setBasePrice] = useState("");
+  const { data: currentBasePrice } = useContractRead({
+    ...astaverdeContractConfig,
+    functionName: "basePrice",
+  });
+  console.log("currentBasePrice", currentBasePrice);
+  const { config } = usePrepareContractWrite({
+    ...astaverdeContractConfig,
+    functionName: "setBasePrice",
+    args: [BigInt(basePrice)],
+  });
+  const { write, isLoading, isSuccess, error } = useContractWrite(config);
+  console.log("setBasePrice", { config, write, isLoading, isSuccess, error });
+
+  const handleSetBasePrice = () => {
+    if (basePrice) {
+      write?.();
+    }
+  };
+
+  return (
+    <ControlContainer title="Set Base Price">
+      <div className="flex items-center mb-4">
+        <input
+          type="number"
+          value={basePrice}
+          onChange={(e) => setBasePrice(e.target.value)}
+          placeholder="Enter Base Price"
+          className="px-4 py-2 mr-2 border border-gray-300 rounded"
+        />
+        <button
+          className="px-4 py-2 bg-secondary text-white rounded hover:bg-blue-700 disabled:opacity-50"
+          disabled={!write || isLoading}
+          onClick={handleSetBasePrice}
+        >
+          Set Base Price
+        </button>
+      </div>
+      {currentBasePrice !== undefined && (
+        <div className="text-gray-500 mb-2">Current Base Price: {currentBasePrice.toString()}</div>
+      )}
+      {isLoading && <div className="text-gray-500">Processing... Please check your wallet.</div>}
+      {isSuccess && <div className="text-primary">Transaction successful</div>}
+      {error && <div className="text-red-500">Error: {error.message}</div>}
+    </ControlContainer>
+  );
+}
+
+function MaxBatchSizeControl() {
+  const [maxMatchSize, setMaxMatchSize] = useState("");
+  const { data: currentMaxMatchSize } = useContractRead({
+    ...astaverdeContractConfig,
+    functionName: "maxBatchSize",
+  });
+  console.log("currentBasePrice", currentMaxMatchSize);
+  const { config } = usePrepareContractWrite({
+    ...astaverdeContractConfig,
+    functionName: "setMaxBatchSize",
+    args: [BigInt(maxMatchSize)],
+  });
+  const { write, isLoading, isSuccess, error } = useContractWrite(config);
+  console.log("setMaxMatchSize", { config, write, isLoading, isSuccess, error });
+
+  const handleSetMaxMatchSize = () => {
+    if (maxMatchSize) {
+      write?.();
+    }
+  };
+
+  return (
+    <ControlContainer title="Set Max Match Size">
+      <div className="flex items-center mb-4">
+        <input
+          type="number"
+          value={maxMatchSize}
+          onChange={(e) => setMaxMatchSize(e.target.value)}
+          placeholder="Enter Max Match Size"
+          className="px-4 py-2 mr-2 border border-gray-300 rounded"
+        />
+        <button
+          className="px-4 py-2 bg-secondary text-white rounded hover:bg-blue-700 disabled:opacity-50"
+          disabled={!write || isLoading}
+          onClick={handleSetMaxMatchSize}
+        >
+          Set Max Match Size
+        </button>
+      </div>
+      {currentMaxMatchSize !== undefined && (
+        <div className="text-gray-500 mb-2">Current Max Match Size: {currentMaxMatchSize.toString()}</div>
+      )}
+      {isLoading && <div className="text-gray-500">Processing... Please check your wallet.</div>}
+      {isSuccess && <div className="text-primary">Transaction successful</div>}
+      {error && <div className="text-red-500">Error: {error.message}</div>}
+    </ControlContainer>
+  );
+}
+
+function AuctionTimeThresholdsControl() {
+  const [dayIncreaseThreshold, setDayIncreaseThreshold] = useState("");
+  const [dayDecreaseThreshold, setDayDecreaseThreshold] = useState("");
+  const { data: currentDayIncreaseThreshold } = useContractRead({
+    ...astaverdeContractConfig,
+    functionName: "dayIncreaseThreshold",
+  });
+  const { data: currentDayDecreaseThreshold } = useContractRead({
+    ...astaverdeContractConfig,
+    functionName: "dayDecreaseThreshold",
+  });
+  const { config } = usePrepareContractWrite({
+    ...astaverdeContractConfig,
+    functionName: "setAuctionTimeThresholds",
+    args: [BigInt(dayIncreaseThreshold), BigInt(dayDecreaseThreshold)],
+  });
+  const { write, isLoading, isSuccess, error } = useContractWrite(config);
+  console.log("setAuctionTimeThresholds", { config, write, isLoading, isSuccess, error });
+
+  const handleSetMaxMatchSize = () => {
+    if (dayIncreaseThreshold && dayDecreaseThreshold) {
+      if (dayIncreaseThreshold < dayDecreaseThreshold) {
+        write?.();
+      } else {
+        alert("Increase threshold must be lower than decrease threshold");
+      }
+    }
+  };
+
+  return (
+    <ControlContainer title="Set Auction Threshold">
+      <div className="flex items-center mb-4">
+        <div className="flex flex-col gap-2">
+          <input
+            type="number"
+            value={dayIncreaseThreshold}
+            onChange={(e) => setDayIncreaseThreshold(e.target.value)}
+            placeholder="Enter Increase Days"
+            className="px-4 py-2 mr-2 border border-gray-300 rounded"
+          />
+          <input
+            type="number"
+            value={dayDecreaseThreshold}
+            onChange={(e) => setDayDecreaseThreshold(e.target.value)}
+            placeholder="Enter Decrease Days"
+            className="px-4 py-2 mr-2 border border-gray-300 rounded"
+          />
+        </div>
+
+        <button
+          className="px-4 py-2 bg-secondary text-white rounded hover:bg-blue-700 disabled:opacity-50"
+          disabled={!write || isLoading}
+          onClick={handleSetMaxMatchSize}
+        >
+          Set Auction Threshold
+        </button>
+      </div>
+
+      {currentDayIncreaseThreshold !== undefined && (
+        <div className="text-gray-500 mb-2">
+          Current Day Increase Threshold: {currentDayIncreaseThreshold.toString()}
+        </div>
+      )}
+      {currentDayDecreaseThreshold !== undefined && (
+        <div className="text-gray-500 mb-2">
+          Current Day Decrease Threshold: {currentDayDecreaseThreshold.toString()}
+        </div>
+      )}
+      {isLoading && <div className="text-gray-500">Processing... Please check your wallet.</div>}
+      {isSuccess && <div className="text-primary">Transaction successful</div>}
+      {error && <div className="text-red-500">Error: {error.message}</div>}
+    </ControlContainer>
+  );
+}
+
+function PlatformPercentageControl() {
+  const [platformSharePercentage, setPlatformSharePercentage] = useState("");
+  const { data: currentPlatformSharePercentage } = useContractRead({
+    ...astaverdeContractConfig,
+    functionName: "platformSharePercentage",
+  });
+  const { config } = usePrepareContractWrite({
+    ...astaverdeContractConfig,
+    functionName: "setPlatformSharePercentage",
+    args: [BigInt(platformSharePercentage)],
+  });
+  const { write, isLoading, isSuccess, error } = useContractWrite(config);
+  console.log("setPlatformSharePercentage", { config, write, isLoading, isSuccess, error });
+
+  const handleSetPlatformSharePercentage = () => {
+    if (platformSharePercentage) {
+      write?.();
+    }
+  };
+
+  return (
+    <ControlContainer title="Set Platform Share Percentage">
+      <div className="flex items-center mb-4">
+        <input
+          type="number"
+          value={platformSharePercentage}
+          onChange={(e) => setPlatformSharePercentage(e.target.value)}
+          placeholder="Enter Platform Share Percentage"
+          className="px-4 py-2 mr-2 border border-gray-300 rounded"
+        />
+        <button
+          className="px-4 py-2 bg-secondary text-white rounded hover:bg-blue-700 disabled:opacity-50"
+          disabled={!write || isLoading}
+          onClick={handleSetPlatformSharePercentage}
+        >
+          Set Platform Share Percentage
+        </button>
+      </div>
+      {currentPlatformSharePercentage !== undefined && (
+        <div className="text-gray-500 mb-2">
+          Current Platform Share Percentage: {currentPlatformSharePercentage.toString()}
+        </div>
+      )}
       {isLoading && <div className="text-gray-500">Processing... Please check your wallet.</div>}
       {isSuccess && <div className="text-primary">Transaction successful</div>}
       {error && <div className="text-red-500">Error: {error.message}</div>}
