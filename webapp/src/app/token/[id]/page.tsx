119e78179a4dcaab36cf702dd0339261ee2e9444
Orpheus Lummis <o@orpheuslummis.info>
2024-01-24 15:45:25 +0700

    fix: Correctly include CIDs in the minting (#58)

--- webapp/src/app/token/[id]/page.tsx
+++ webapp/src/app/token/[id]/page.tsx
@@ -1,14 +1,38 @@
-import React from "react";
+"use client";
 
-export default function Page({ params }: { params: { id: string } }) {
-  if (typeof params.id !== "string") {
+import React, { useEffect, useState } from "react";
+import { useContractRead } from "wagmi";
+import { astaverdeContractConfig } from "../../../lib/contracts";
+
+export default function Page({ params }: { params: { id: bigint } }) {
+
+  const {
+    data,
+    isError,
+    isLoading,
+    error: lastBatchIDError,
+  } = useContractRead({
+    ...astaverdeContractConfig,
+    functionName: "tokens",
+    args: [params.id],
+  });
+
+
+  if (isLoading) {
     return <p>Loading...</p>;
+  } else if (isError) {
+    return <p>Error: {lastBatchIDError && lastBatchIDError.message}</p>;
   }
 
   return (
-    <div>
-      <h1>Token: {params.id}</h1>
-      <p>This is a simple page to view one token.</p>
+    <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}>
+      <div style={{ backgroundColor: "#f0f0f0", padding: "20px", borderRadius: "5px" }}>
+        <h1>Token: {params.id.toString()}</h1>
+        <p>tokenId: {data && data[0].toString()}</p>
+        <p>producer: {data && data[1]}</p>
+        <p>cid: {data && data[2]}</p>
+        <p>isRedeemed: {data && data[3]}</p>
+      </div>
     </div>
   );
-}
+} 
