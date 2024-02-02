100445f6be47988f23e55338065a1b8d6c1507e8
aeither <36173828+aeither@users.noreply.github.com>
2024-02-02 14:40:29 +0800

    About page Fixes #23

--- webapp/src/app/about/page.tsx
+++ webapp/src/app/about/page.tsx
@@ -14,17 +14,22 @@ export default function Page() {
             producers with individuals and organizations eager to contribute to global carbon reduction. By making each
             token a claimable representation of a real-world environmental benefit, EcoTradeZone fosters a new,
             eco-conscious economy where sustainable practices are both recognized and rewarded.
+            <br />
+            <a href="#" className="underline text-blue-500">
+              *Read Everything about EcoAssets before buying
+            </a>
           </p>
         </div>
         <div>
           <h2 className="font-bold text-xl mb-4">Bug Report</h2>
           <p className="mb-4">
             Please tell us about any bugs you find, we want to keep this place running smoothly to help producers get
-            their earnings
+            their earnings. There may be some small rewards for bugs found, depending on the bug.
+            <br />
+            Email: chris@bionerg.com
+            <br />
+            Title: Bug Found
           </p>
-          <a href="#" className="underline text-blue-500">
-            Bug report link
-          </a>
         </div>
         <h2 className="font-bold text-xl mb-4">FAQ</h2>
         <div>
@@ -35,6 +40,28 @@ export default function Page() {
             to it in terms of its data integrity. But this means that you need the currencies that are used on this Base
             network. Coinbase wallets work natively with Base and the currency that you purchase in is USDC so make sure
             that the currency you have on your base network wallet.
+            <br />
+            Q: What do I need to know about using my wallet here?
+            <br />
+            A: Transactions here are conducted using Base native USDC, issued by Circle. For more understanding:
+            <br />
+            <a
+              href="https://base.mirror.xyz/Ouwm--AtTIVyz40He3FxI0fDAC05lOQwN6EzFMD_2UM"
+              className="underline text-blue-500"
+            >
+              Learn about Base
+            </a>
+            <br />
+            <a href="https://help.coinbase.com/en/coinbase/other-topics/other/base" className="underline text-blue-500">
+              Base from Coinbase
+            </a>
+            <br />
+            <a
+              href="https://www.circle.com/blog/usdc-now-available-natively-on-base"
+              className="underline text-blue-500"
+            >
+              USDC on Base
+            </a>
           </p>
         </div>
       </div>
