c7026fd4c4103c7e2bc10a1cd0b0aece4d226583
aeither <36173828+aeither@users.noreply.github.com>
2024-02-02 14:21:54 +0800

    fix: add Privacy Policy Page Fixes #86

---
+++ webapp/src/app/privacy/page.tsx
@@ -0,0 +1,50 @@
+export default function Page() {
+  return (
+    <div className="pt-4 flex flex-col gap-4">
+      <h1 className="text-xl font-bold">EcoTradeZone of Bionerg Ltd. Privacy Policy</h1>
+      <p className="text-sm">Date: 31 Jan 2024</p>
+      <section className="space-y-2">
+        <h2 className="text-lg font-semibold">Our contact details</h2>
+        <p>Name: Bionerg Ltd.</p>
+        <p>Address: 4th Floor, 5B The Parklands, Lostock, Bolton, BL6 4SD</p>
+        <p>Company number: 9139423</p>
+        <p>
+          E-mail:{" "}
+          <a href="mailto:chris@bionerg.com" className="text-blue-600 hover:underline">
+            chris@bionerg.com
+          </a>
+        </p>
+      </section>
+      <div className="pt-4 flex gap-4 flex-col">
+        The type of personal information we collect We do not consider public wallet addresses to be personal
+        information. We currently collect and process the following information: email addresses from voluntary sign ups
+        for alerts or bug communications How we get the personal information and why we have it Most of the personal
+        information we process is provided to us directly by you for one of the following reasons: to fulfil voluntary
+        requests for auction alerts or bug communications. We do not receive personal information indirectly. We use the
+        information that you have given us in order to alert you to auctions if requested, or respond to your bug
+        submission if requested. We may share this information with our programming partners to resolve bugs. Under the
+        UK General Data Protection Regulation (UK GDPR), the lawful bases we rely on for processing this information
+        are: Your consent. You are able to remove your consent at any time. You can do this by contacting
+        chris@bionerg.com for removal from auction alerts chris@bionerg.com for removal from further bug discussionHow
+        we store your personal information Your email address, if voluntarily given, is kept in a gmail held account
+        with 2FA. We keep emails for one year. We will then dispose your information by clearing those email accounts
+        which are the sole record of your email address. Your data protection rights Under data protection law, you have
+        rights including: Your right of access - You have the right to ask us for copies of your personal information.
+        Your right to rectification - You have the right to ask us to rectify personal information you think is
+        inaccurate. You also have the right to ask us to complete information you think is incomplete. Your right to
+        erasure - You have the right to ask us to erase your personal information in certain circumstances. Your right
+        to restriction of processing - You have the right to ask us to restrict the processing of your personal
+        information in certain circumstances. Your right to object to processing - You have the the right to object to
+        the processing of your personal information in certain circumstances. Your right to data portability - You have
+        the right to ask that we transfer the personal information you gave us to another organisation, or to you, in
+        certain circumstances. You are not required to pay any charge for exercising your rights. If you make a request,
+        we have one month to respond to you. Please contact us at chris@bionerg.com for auction alert request removal,
+        or chris@bionerg.com for bug report communication removal if you wish to make a request. How to complain If you
+        have any concerns about our use of your personal information, you can make a complaint to us at
+        chris@bionerg.com. You can also complain to the ICO if you are unhappy with how we have used your data. The
+        ICO’s address: Information Commissioner’s Office Wycliffe House Water Lane Wilmslow Cheshire SK9 5AF Helpline
+        number: 0303 123 1113 ICO website: https://www.ico.org.uk/
+      </div>
+    </div>
+  );
+}
