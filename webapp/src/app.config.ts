914add9d92def61aeabb114dcf322da22aaf6b83
Orpheus Lummis <o@orpheuslummis.info>
2024-02-12 18:37:49 +0700

    adjust frontend and contract addresses as well

--- webapp/src/app.config.ts
+++ webapp/src/app.config.ts
@@ -1,12 +1,14 @@
 export const metadata = {
-  title: "wagmi",
+	title: "wagmi",
 };
 
+export const USDC_DECIMALS = 6;
+
 export const navigationLinks = [
-  { name: "Everything about EcoAssets", url: "/ecoassets" },
-  { name: "Market", url: "/" },
-  { name: "Redeem", url: "/mytokens" },
-  { name: "About", url: "/about" },
+	{ name: "Everything about EcoAssets", url: "/ecoassets" },
+	{ name: "Market", url: "/" },
+	{ name: "Redeem", url: "/mytokens" },
+	{ name: "About", url: "/about" },
 ];
 
 export const IPFS_GATEWAY_URL = "https://ipfs.io/ipfs/";
