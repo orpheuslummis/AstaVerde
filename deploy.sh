0d437006e15cef073728af4c2c3ecf03de862275
Orpheus Lummis <o@orpheuslummis.info>
2024-02-20 10:16:11 -0800

    ok

--- deploy.sh
+++ deploy.sh
@@ -6,28 +6,29 @@
 
 # Client parameters (EDIT THIS HERE! 🌸)
 NETWORK="base-sepolia"
-USDC_ADDRESS="tbd"
-IMAGE_FOLDER="example_nftdata"
-CSV_PATH="example_nftdata/nft_metadata.csv"
-ALCHEMY_APIKEY="your-api-key"
-PRIVATE_KEY="your-private-key"
+# USDC_ADDRESS="tbd" # if on mainnet
+ALCHEMY_APIKEY="tbd"
+PRIVATE_KEY="tbd"
 
 if ! command -v docker &> /dev/null; then
     echo "Docker is not installed. Please install Docker to proceed."
     exit 1
 fi
 
-docker build -t astaverde .
+IMAGE_NAME="astaverde"
+IMAGE_EXISTS=$(docker images -q $IMAGE_NAME)
+if [ -z "$IMAGE_EXISTS" ]; then
+    docker build -t $IMAGE_NAME .
+else
+    echo "Image $IMAGE_NAME already exists."
+fi
 
 echo "Deploying on the $NETWORK network..."
 
 docker run --rm \
     -e NETWORK=$NETWORK \
     -e CONTRACT_ADDRESS=$USDC_ADDRESS \
-    -e IMAGE_FOLDER=$IMAGE_FOLDER \
-    -e CSV_PATH=$CSV_PATH \
     -e ALCHEMY_APIKEY=$ALCHEMY_APIKEY \
-    -e EMAIL=$EMAIL \
     -e PRIVATE_KEY=$PRIVATE_KEY \
     astaverde npm run deploy:contracts -- --network $NETWORK
 
--- mint.sh
+++ mint.sh
@@ -7,20 +7,33 @@
 # Client parameters (EDIT THIS HERE! 🌸)
 NETWORK="base-sepolia"
 CONTRACT_ADDRESS="tbd"
-IMAGE_FOLDER="example_nftdata"
+NFTDATA_PATH="example_nftdata"
+IMAGE_FOLDER="example_nftdata/images"
 CSV_PATH="example_nftdata/nft_metadata.csv"
-ALCHEMY_APIKEY="your-api-key"
-EMAIL="your-email"
-PRIVATE_KEY="your-private-key"
+ALCHEMY_APIKEY="tbd"
+EMAIL="tbd"
+PRIVATE_KEY="tbd"
+
+if [ ! -d "$NFTDATA_PATH" ]; then
+    echo "Error: Directory $NFTDATA_PATH not found."
+    exit 1
+fi
 
 if ! command -v docker &> /dev/null; then
     echo "Docker is not installed. Please install Docker to proceed."
     exit 1
 fi
 
-docker build -t astaverde .
+IMAGE_NAME="astaverde"
+IMAGE_EXISTS=$(docker images -q $IMAGE_NAME)
+if [ -z "$IMAGE_EXISTS" ]; then
+    docker build -t $IMAGE_NAME .G
+else
+    echo "Image $IMAGE_NAME already exists."
+fi
 
 echo "Minting NFTs on the $NETWORK network, with contract $CONTRACT_ADDRESS..."
+echo "Using the metadata from $CSV_PATH and images from $IMAGE_FOLDER/..."
 
 docker run --rm \
     -e NETWORK=$NETWORK \
@@ -30,6 +43,7 @@ docker run --rm \
     -e ALCHEMY_APIKEY=$ALCHEMY_APIKEY \
     -e EMAIL=$EMAIL \
     -e PRIVATE_KEY=$PRIVATE_KEY \
+    -v $(pwd)/"$NFTDATA_PATH":/app/"$NFTDATA_PATH" \
     astaverde npm run task:mint
 
 echo "Minting process completed."
