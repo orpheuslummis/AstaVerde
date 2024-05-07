7c6e418171f9cd42f0e5905882fb98accc254962
Orpheus Lummis <o@orpheuslummis.info>
2024-05-07 11:15:08 -0400

    Rename Redeem page My EcoAssets #133

--- webapp/src/app.config.ts
+++ webapp/src/app.config.ts
@@ -1,18 +1,18 @@
 export const metadata = {
-	title: "wagmi",
+  title: "wagmi",
 };
 
 export const USDC_DECIMALS = 6;
 
 export const navigationLinks = [
-	{ name: "Everything about EcoAssets", url: "/ecoassets" },
-	{ name: "Market", url: "/" },
-	{ name: "Redeem", url: "/mytokens" },
-	{ name: "About", url: "/about" },
+  { name: "Everything about EcoAssets", url: "/ecoassets" },
+  { name: "Market", url: "/" },
+  { name: "My EcoAssets", url: "/mytokens" },
+  { name: "About", url: "/about" },
 ];
 
 export const IPFS_GATEWAY_URL = "https://gateway.pinata.cloud/ipfs/";
 
-export const CHAIN_SELECTION =
-	process.env.NEXT_PUBLIC_CHAIN_SELECTION || process.env.CHAIN_SELECTION;
+export const CHAIN_SELECTION = process.env.NEXT_PUBLIC_CHAIN_SELECTION ||
+  process.env.CHAIN_SELECTION;
 console.log("config: CHAIN_SELECTION", CHAIN_SELECTION);
