9edb652682a6490bd688997cad3c917eaeb7c6ad
Orpheus Lummis <o@horizonomega.org>
2024-08-21 19:20:25 -0500

    simplify nextjs config

--- webapp/next.config.js
+++ webapp/next.config.js
@@ -8,21 +8,6 @@ module.exports = {
     config.resolve.alias['@'] = path.join(__dirname, 'src', '@');
     return config;
   },
-  async headers() {
-    return [
-      {
-        source: '/favicon.ico',
-        headers: [
-          {
-            key: 'Content-Type',
-            value: 'image/x-icon',
-          },
-        ],
-      },
-    ];
-  },
-  poweredByHeader: false,
-  swcMinify: true,
   images: {
     domains: ['gateway.pinata.cloud'],
   },
