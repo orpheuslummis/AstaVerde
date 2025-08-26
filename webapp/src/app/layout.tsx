c429a388e7f47c8263ab66efbfc837d823ee2d5f
Orpheus Lummis <o@orpheuslummis.info>
2025-08-26 21:07:27 +0000

    feat(webapp): add error boundaries and wire them in layout

--- webapp/src/app/layout.tsx
+++ webapp/src/app/layout.tsx
@@ -5,6 +5,7 @@ import { Footer } from "../components/Footer";
 import { Header } from "../components/Header";
 import { OnboardingModal } from "../components/OnboardingModal";
 import { Providers } from "../components/Providers";
+import ErrorBoundary from "../components/ErrorBoundary";
 
 export const metadata = {
   title: "AstaVerde - Carbon Offset Marketplace",
@@ -17,12 +18,18 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en">
       <body className="flex flex-col min-h-screen">
         <Providers>
-          <OnboardingModal />
-          <Header links={navigationLinks} />
-          <div className="flex-grow container">
-            <main>{children}</main>
-          </div>
-          <Footer />
+          <ErrorBoundary>
+            <OnboardingModal />
+            <Header links={navigationLinks} />
+            <div className="flex-grow container">
+              <main>
+                <ErrorBoundary>
+                  {children}
+                </ErrorBoundary>
+              </main>
+            </div>
+            <Footer />
+          </ErrorBoundary>
           <Toaster
             position="top-center"
             gutter={8}
